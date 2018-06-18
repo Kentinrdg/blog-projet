1a429284559bce93cf0cb8755625e34d016d10eb
Kentinrdg <Kentinrdg@users.noreply.github.com>
2018-06-18 10:55:43 +0200

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'mon blog';
+  @Input() titlePost: string;
+  @Input() contentPost: string;
+
+  comments=[
+  	{
+  	date: new Date(), 
+    title:"My Title", 
+    comments:"MyComment"
+  	},
+  ];
+
+  post={date: null, title:"", comments:""};
+
+    lastUpdate = new Promise((resolve, reject) => {
+
+  	const date = new Date();
+  	setTimeout(
+  		() => {
+  			resolve(date);
+  	}, 2000
+  	);
+});
+
+		onAddPost(){
+      this.post.date = new Date();
+			this.comments.push(this.post);
+		}
 }
